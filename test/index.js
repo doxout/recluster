e50b940e5f7e4760f2d8694bb389b6ad97111e19
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-04-29 15:30:02 +0200

    switched to tap for tests, bump version 0.1.5

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 var nlb = require('../index.js'),
     path = require('path'),
     http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    tap = require('tap');
 
 var balancer = null;
 
@@ -32,7 +33,7 @@ function setServer(file, done) {
 }
 
 
-exports.setUp = function(done) {
+function setUp(t) {
     setServer('server-ok.js', function(err) {
         if (err) throw err;
         balancer = nlb(path.join(__dirname, 'lib', 'server.js'), {
@@ -40,70 +41,79 @@ exports.setUp = function(done) {
             workers:2, 
             timeout: 0.6
         });
-        balancer.once('listening', function(){ done(); });
+        balancer.once('listening', function(){ t.end(); });
         balancer.run();
     });
 }
 
+function tearDown(t) {
+    balancer.terminate();
+    t.end();
+}
 
-exports["broken server"] = function(t) {
-    t.expect(3);
+
+function runTest(desc, testfn) {
+    tap.test(desc, function(t) {
+        t.test('setup', setUp)
+        t.test(desc, testfn);
+        t.test('teardown', tearDown);
+    });
+};
+
+
+runTest("broken server", function(t) {
+    t.plan(3);
     setServer('server-broken.js', function(err) {
-        t.ok(!err, "Error changing to broken server");
+        t.ok(!err, "changing to broken server");
         balancer.reload();
         setTimeout(setServer.bind(this, 'server-ok.js', afterOk), 300);
         function afterOk(err) {
-            t.ok(!err, "Error changing to okay server");
+            t.ok(!err, "changing to okay server");
             setTimeout(function() { 
                 request({
                     url: 'http://localhost:8000/1'
                 }, function(err) {
                     t.ok(!err, "Response received");
-                    t.done();
+                    t.end();
                 }); 
             }, 300);
         }
     });  
-};
+});
 
 
 
-exports["reload in the middle of a request"] = function(t) {
-    t.expect(1);
+runTest("reload in the middle of a request", function(t) {
+    t.plan(1);
     request({url: 'http://localhost:8000/100'}, function(err) {
         t.ok(!err, "Response received");
-        t.done();
+        t.end();
     });
     setTimeout(balancer.reload.bind(balancer), 50);
-}
+});
 
-exports["server with an endless setInterval"] = function(t) {
-    t.expect(2);
+runTest("server with an endless setInterval", function(t) {
+    t.plan(2);
     setServer('server-unclean.js', function(err) {
         t.ok(!err, "Changing to unending server");
         balancer.reload();
         setTimeout(balancer.reload.bind(balancer), 100);
         setTimeout(function() {
             var workerIds = Object.keys(require('cluster').workers);
-            t.equals(workerIds.length, 2, "Two workers are active");
-            t.done(); // this test will never end
+            t.equal(workerIds.length, 2, "Two workers are active");
+            t.end(); // this test will never end
         }, 900)
         
     })
-}
+});
 
-exports["simple balancer"] = function(t) {
-    t.expect(1);
+runTest("simple balancer", function(t) {
+    t.plan(1);
     request({url: 'http://localhost:8000/1'}, function(err) {
         t.ok(!err, "Response received");
-        t.done();
+        t.end();
     });
+});
 
-}
 
-
-exports.tearDown = function(done) {
-    balancer.terminate();
-    done();
-}
 
